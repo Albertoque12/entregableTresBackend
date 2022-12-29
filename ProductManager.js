56eff54904e8c429b6f5a3e2872d503c1f744951
Alberto Orozco <kirarap@hotmail.com>
2022-12-29 13:19:02 -0800

    Subiendo tercera practica de comision de backend

---
+++ ProductManager.js
@@ -0,0 +1,15 @@
+const fs = require("fs")
+
+
+class ProductManager {
+    constructor(filePath){
+        this.filePath = filePath
+    }
+
+    getProducts(){
+        return JSON.parse(fs.readFileSync(this.filePath, 'utf-8'))
+    }
+}
+
+module.exports = ProductManager
+
